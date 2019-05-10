8efa6bcce7cb456969197ec9810a680a08dc26ee
hreeves19 <hreeves@islander.tamucc.edu>
2019-05-10 16:18:25 -0500

    Add project files.

---
+++ DocumentManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DocumentManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
